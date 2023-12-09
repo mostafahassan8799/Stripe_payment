ba7fbc20b3dc7aef879dd3ab2124a4dccfb46d30
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-12-09 17:01:22 +0200

    add payment states

--- lib/Features/checkout/presentation/manger/cubit/payment_state.dart
+++ lib/Features/checkout/presentation/manger/cubit/payment_state.dart
@@ -4,3 +4,10 @@ part of 'payment_cubit.dart';
 sealed class PaymentState {}
 
 final class PaymentInitial extends PaymentState {}
+final class PaymentLoading extends PaymentState {}
+final class PaymentSuccess extends PaymentState {}
+final class PaymentFailure extends PaymentState {
+  final String errMessage;
+
+  PaymentFailure({required this.errMessage});
+}
