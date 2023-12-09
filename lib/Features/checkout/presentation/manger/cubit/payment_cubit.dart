b75e6c0807a8306226962f9a4695bedb2d78ea44
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-12-09 17:07:40 +0200

    handle cubit for payment

--- lib/Features/checkout/presentation/manger/cubit/payment_cubit.dart
+++ lib/Features/checkout/presentation/manger/cubit/payment_cubit.dart
@@ -1,8 +1,27 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/repos/checkout_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'payment_state.dart';
 
 class PaymentCubit extends Cubit<PaymentState> {
-  PaymentCubit() : super(PaymentInitial());
+  PaymentCubit(this.checkoutRepo) : super(PaymentInitial());
+  final CheckoutRepo checkoutRepo;
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(PaymentLoading());
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+    data.fold((l) => emit(PaymentFailure(errMessage: l.errorMessage)),
+        (r) => emit(PaymentSuccess()));
+  }
+
+  @override
+  void onChange(Change<PaymentState> change) {
+    log(change.toString());
+    super.onChange(change);
+  }
 }
